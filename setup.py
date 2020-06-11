219e4a7942054cb608ee0b25e708ce8da526fbc7
Kristoffer Bäckman <kristoffer.backman@gmail.com>
2020-06-11 07:14:01 +0200

    add control table

--- setup.py
+++ setup.py
@@ -20,10 +20,22 @@ CREATE TABLE 'decibel_minute_data' (
 'time' TEXT,
 'db_min' NUMERIC,
 'db_mean' NUMERIC,
+'db_median' NUMERIC,
 'db_max' NUMERIC)
 """
 
+# create a table to store the pid of the script when it is run, this can then
+# be used to check if the script is still running.
+
+pids = """
+CREATE TABLE 'last_pid' (
+'key' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+'time' TEXT,
+'pid' NUMERIC)
+"""
+
 cur.execute(decibel)
+cur.execute(pids)
 sound_conn.commit()
 sound_conn.close()
 
