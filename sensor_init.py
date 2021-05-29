dd8afdb1cc86850249c02c993fe3dd3740b229c6
Kristoffer Bäckman <kristoffer.backman@gmail.com>
2021-05-29 19:52:05 +0200

    sensor init only affecting air qual

--- sensor_init.py
+++ sensor_init.py
@@ -6,7 +6,7 @@ import confs
 from tinkerforge.ip_connection import IPConnection
 
 from tinkerforge.brick_master import BrickMaster
-from tinkerforge.bricklet_sound_pressure_level import BrickletSoundPressureLevel
+# from tinkerforge.bricklet_sound_pressure_level import BrickletSoundPressureLevel
 from tinkerforge.bricklet_air_quality import BrickletAirQuality
 
 # Create IP connection
@@ -14,7 +14,7 @@ ipcon = IPConnection()
 
 # bricks and bricklets
 master = BrickMaster(confs.MASTER_UID, ipcon)
-spl = BrickletSoundPressureLevel(confs.SOUND_UID, ipcon)
+# spl = BrickletSoundPressureLevel(confs.SOUND_UID, ipcon)
 air = BrickletAirQuality(confs.AIR_UID, ipcon)
 
 # connect
@@ -23,7 +23,7 @@ ipcon.connect(confs.HOST, confs.PORT)
 # turn on the leds
 
 master.enable_status_led()
-spl.set_status_led_config(1)
+# spl.set_status_led_config(1)
 air.set_status_led_config(1)
 
 # sleepa while
@@ -31,7 +31,7 @@ time.sleep(10)
 
 # turn them off
 master.disable_status_led()
-spl.set_status_led_config(0)
+# spl.set_status_led_config(0)
 air.set_status_led_config(0)
 
 # when done dsiconnect
